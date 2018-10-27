39a855bdf3e9e68c082aa0b55519abd2143a12bc
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2018-10-27 20:10:03 +0200

    started on the customizer for the about section

--- inc/customizer.php
+++ inc/customizer.php
@@ -5,17 +5,40 @@ function cd_customizer_settings( $wp_customize ) {
         'title'      => 'Colors',
         'priority'   => 30,
     ));
-
     $wp_customize->add_setting( 'background_color' , array(
         'default'     => '#FFFFFF',
         'transport'   => 'postMessage',
     ));
-
     $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'background_color', array(
-        'label'        => 'Background Color',
+        'label'      => 'Background Color',
         'section'    => 'cd_colors',
         'settings'   => 'background_color',
     )));
+
+    $wp_customize->add_section( 'about_me' , array(
+        'title'      => 'About',
+        'priority'   => 30,
+    ));
+    $wp_customize->add_setting( 'left_column' , array(
+        'default'     => 'placeholder',
+        'transport'   => 'postMessage',
+    ));
+    $wp_customize->add_control( 'left_column_control', array(
+        'label'    => 'Linker kolom',
+        'type'     => 'textarea',
+        'section'  => 'about_me',
+        'settings' => 'left_column'
+    ));
+    $wp_customize->add_setting( 'right_column' , array(
+        'default'     => 'placeholder',
+        'transport'   => 'postMessage',
+    ));
+    $wp_customize->add_control( 'right_column_control', array(
+        'label'    => 'Rechter kolom',
+        'type'     => 'textarea',
+        'section'  => 'about_me',
+        'settings' => 'right_column'
+    ));
 }
 
 add_action( 'customize_register', 'cd_customizer_settings' );
--- js/customizer.js
+++ js/customizer.js
@@ -5,5 +5,17 @@
 			$( 'body' ).css( 'background-color', newval );
 		} );
 	} );
+
+	wp.customize( 'left_column', function( value ) {
+		value.bind( function( newval ) {
+			$( '#left-column > p' ).html( newval );
+		} );
+	} );
+
+	wp.customize( 'right_column', function( value ) {
+		value.bind( function( newval ) {
+			$( '#right-column > p' ).html( newval );
+		} );
+	} );
     
 } )( jQuery );
